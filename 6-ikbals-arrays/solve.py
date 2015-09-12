14a03429f982c643a6ab38b7f45844f95f7b0b6f
Christos Tsirigotis <tsirif@gmail.com>
2015-09-12 05:12:17 +0300

    Honorable try to split space before acting the commands

--- 6-ikbals-arrays/solve.py
+++ 6-ikbals-arrays/solve.py
@@ -1,19 +1,14 @@
 #  import defaultdict as dd
 import bisect as bs
 
+
 def update_blocks(blocks, new_l, new_r, c, d):
     i_l = bs.bisect_right(blocks, [new_l])
     i_r = bs.bisect_right(blocks, [new_r])
+    #print blocks, i_r, i_l
     for i in xrange(i_l, i_r):
         blocks[i][1] += c%md
         blocks[i][2] += d%md
-    if blocks[i_l][0] == new_l:
-        if blocks[i_r][0] != new_r:
-            blocks.insert(i_r, [new_r, blocks[i_r - 1][1]-c, blocks[i_r - 1][2]-d])
-    else:
-        blocks.insert(i_l, [new_l, blocks[i_l - 1][1]+c, blocks[i_l - 1][2]+d])
-        if blocks[i_r+1][0] != new_r:
-            blocks.insert(i_r+1, [new_r, blocks[i_r][1]-c, blocks[i_r][2]-d])
 
 def q1(blocks, new_l, new_r, c):
     update_blocks(blocks, new_l, new_r+1, c, 0)
@@ -28,21 +23,38 @@ def q3(blocks, new_l, new_r):
     summ = 0
     for i in xrange(i_l, i_r):
         summ += (blocks[i][1] * blocks[i][2] * (blocks[i+1][0]-blocks[i][0]))%md
+        summ = summ % md
     if blocks[i_l][0] != new_l:
         summ += (blocks[i_l-1][1] * blocks[i_l-1][2] * (blocks[i_l][0]-new_l))%md
+        summ = summ % md
     if blocks[i_r][0] != new_r:
-        summ = (summ+md-blocks[i_r-1][1] * blocks[i_r-1][2] * (blocks[i_r][0]-new_r))%md
-    print summ%md
+        summ = (summ - blocks[i_r-1][1] * blocks[i_r-1][2] * (blocks[i_r][0]-new_r))%md
+    print summ
 
 md = 1000000007
 
 n,q = map(int,raw_input().split())
-aa = [[0,0,0], [n, 0, 0]]
+aa = []
+commands = []
+lrs = set([0, n])
 for i in xrange(q):
-    qq = map(int, raw_input().split())
-    if qq[0] == 1:
-        q1(aa, qq[1]-1, qq[2]-1, qq[3])
-    if qq[0] == 2:
-        q2(aa, qq[1]-1, qq[2]-1, qq[3])
-    if qq[0] == 3:
+    commands.append(map(int, raw_input().split()))
+for c in commands:
+    if c[0] != 3:
+        lrs.add(c[1]-1)
+        lrs.add(c[2])
+for l in sorted(lrs):
+    aa.append([l, 0, 0])
+appends = []
+for qq in commands:
+    if qq[0] != 3:
+        appends.append(qq)
+    else:
+        appends.sort(key=lambda x: x[2]-x[1], reverse=True)
+        for qqq in appends:
+            if qqq[0] == 1:
+                q1(aa, qqq[1]-1, qqq[2]-1, qqq[3])
+            if qqq[0] == 2:
+                q2(aa, qqq[1]-1, qqq[2]-1, qqq[3])
+        appends = []
         q3(aa, qq[1]-1, qq[2]-1)
